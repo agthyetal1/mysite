740366cf13d1900dc5dc10068ca016b991173d2f
agth <13995801370@qq.com>
2018-04-04 10:28:22 +0800

    修改

--- src/main/java/com/oil/action/ToolAction.java
+++ src/main/java/com/oil/action/ToolAction.java
@@ -8,9 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Controller
 @RequestMapping("/tool")
 public class ToolAction extends CommAction {
+
+    public static void main(String[] args) {
+        Matcher m = Pattern.compile("number\\(([0-9]+)\\)").matcher("adfsdnumber(334434)");
+        System.out.println(m.matches());
+        System.out.println(m.group(0));
+        System.out.println(m.group(1));
+//        System.out.println(m.group(2));
+        System.out.println(m.groupCount());
+        System.out.println();
+
+
+    }
 
     @RequestMapping(value = "/ddl2bean.do",method = RequestMethod.POST)
     @ResponseBody
@@ -18,7 +33,8 @@ public class ToolAction extends CommAction {
         if(ddl == null) {
             return failure("ddl不能为空");
         }
-        ddl = ddl.replace("(15,4)", "(15_4)");
+        ddl = ddl.toLowerCase();
+        ddl = ddl.replaceAll("\\(([0-9]+),([0-9]+)\\)", "($1_$2)");
         String[] ddls = ddl.split(",");
         StringBuilder bean = new StringBuilder();
         StringBuilder mapper = new StringBuilder();
@@ -30,15 +46,33 @@ public class ToolAction extends CommAction {
                 continue;
             }
             String col = str2[0];
-            col = col.toLowerCase();
             String type = str2[1];
-            if(type.toLowerCase().startsWith("number(1)") || type.toLowerCase().startsWith("number(2)") || type.toLowerCase().startsWith("number(3)")) {
-                type = "Integer";
-            } else if(type.toLowerCase().startsWith("number(15_4)")) {
-                type = "Double";
-            } else if(type.toLowerCase().startsWith("number")) {
+            if(type.matches("^number\\([0-9]+\\)")) {
+                Matcher m = Pattern.compile("number\\(([0-9]+)\\)").matcher(type);
+                if(Integer.parseInt(m.group(1)) > 10) {
+                    type = "Long";
+                } else {
+                    type = "Integer";
+                }
+            } else if(type.matches("^number\\(([0-9]+)_([0-9]+)\\)")) {
+                Matcher m = Pattern.compile("number\\(([0-9]+)_([0-9]+)\\)").matcher(type);
+                System.out.println(m.matches());
+                System.out.println(m.group(0));
+                System.out.println(m.group(1));
+                System.out.println(m.group(2));
+                System.out.println(m.groupCount());
+                if(Integer.parseInt(m.group(2)) == 0) {
+                    if(Integer.parseInt(m.group(1)) <= 10) {
+                        type = "Integer";
+                    } else {
+                        type = "Long";
+                    }
+                } else {
+                    type = "Double";
+                }
+            } else if(type.startsWith("number")) {
                 type = "Long";
-            } else if(type.toLowerCase().startsWith("timestamp") || type.toLowerCase().startsWith("date")) {
+            } else if(type.startsWith("timestamp") || type.startsWith("date")) {
                 type = "Date";
             } else {
                 type = "String";
